c5e9bd4b414c298b7b3ca74f44aef141eff9ff3f
Wahab <Wahabasifdeveloper@gmail.com>
2023-05-31 16:24:45 +0500

    change password update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -118,37 +118,55 @@ class AuthController extends Controller
 
     public function changePass(Request $request)
     {
-        $input = $request->all();
-        $userid = Auth::guard('api')->user()->id;
-        $rules = array(
+        $validator = Validator::make($request->all(), [
             'old_password' => 'required',
             'new_password' => 'required|min:8',
             'confirm_password' => 'required|same:new_password',
-        );
-        $validator = Validator::make($input, $rules);
+        ]);
+    
         if ($validator->fails()) {
-            $arr = array("status" => 400, "message" => $validator->errors()->first(), "data" => array());
-        } else {
-            try {
-                if ((Hash::check(request('old_password'), Auth::user()->password)) == false) {
-                    $arr = array("status" => 400, "message" => "Check your old password.", "data" => array());
-                } else if ((Hash::check(request('new_password'), Auth::user()->password)) == true) {
-                    $arr = array("status" => 400, "message" => "Please enter a password which is not similar then current password.", "data" => array());
-                } else {
-                    User::where('id', $userid)->update(['password' => Hash::make($input['new_password'])]);
-                    $arr = array("status" => 200, "message" => "Password updated successfully.", "data" => array());
-                }
-            } catch (\Exception $ex) {
-                if (isset($ex->errorInfo[2])) {
-                    $msg = $ex->errorInfo[2];
-                } else {
-                    $msg = $ex->getMessage();
-                }
-                $arr = array("status" => 400, "message" => $msg, "data" => array());
-            }
+            return response()->json([
+                "status" => 400,
+                "message" => $validator->errors()->first(),
+                "data" => []
+            ]);
         }
-        return \Response::json($arr);
+    
+        $user = Auth::guard('api')->user();
+    
+        if (!Hash::check($request->input('old_password'), $user->password)) {
+            return response()->json([
+                "status" => 400,
+                "message" => "Check your old password.",
+                "data" => []
+            ]);
+        }
+    
+        if (Hash::check($request->input('new_password'), $user->password)) {
+            return response()->json([
+                "status" => 400,
+                "message" => "Please enter a password that is not similar to the current password.",
+                "data" => []
+            ]);
+        }
+    
+        try {
+            $user->update(['password' => Hash::make($request->input('new_password'))]);
+            return response()->json([
+                "status" => 200,
+                "message" => "Password updated successfully.",
+                "data" => []
+            ]);
+        } catch (\Exception $ex) {
+            $msg = isset($ex->errorInfo[2]) ? $ex->errorInfo[2] : $ex->getMessage();
+            return response()->json([
+                "status" => 400,
+                "message" => $msg,
+                "data" => []
+            ]);
+        }
     }
+    
 
     function send(Request $request)
     {
